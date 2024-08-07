c1aedb22832083d9744e4396cb87d9449306ddd6
JayR <atamosajr@yahoo.com>
2024-08-07 11:36:09 +0800

    Added the xcode project

---
+++ CarTest/CarTest/CarTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarTestApp.swift
+//  CarTest
+//
+//  Created by JayR Atamosa on 8/7/24.
+//
+
+import SwiftUI
+
+@main
+struct CarTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
